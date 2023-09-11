c3a37ed85f30c5d922484aab43d66d4ed12ef7fa
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-09-11 17:57:42 +0900

    <hotfix>

--- src/views/content/OrderView.vue
+++ src/views/content/OrderView.vue
@@ -130,7 +130,7 @@ export default {
 .title {
   font-size: 2.4rem;
   font-weight: bold;
-  width: 50%;
+  width: 45rem;
   margin: 0 auto;
   margin-bottom: 2rem;
 }
@@ -142,7 +142,7 @@ export default {
 
 .date {
   font-size: 1.2rem;
-  width: 50%;
+  width: 45rem;
   margin: 0 auto;
   margin-bottom: 1.5rem;
   text-align: left;
@@ -150,10 +150,9 @@ export default {
 
 .box {
   border: solid 1px rgba(128, 128, 128, 0.168);
-  width: 35rem;
+  width: 45rem;
   margin: 0 auto;
   margin-bottom: 2rem;
-  border-radius: 8px;
 }
 .line1 {
   margin-top: 0.5rem;
@@ -193,9 +192,9 @@ export default {
   text-align: center;
   border-radius: 8px;
   cursor: pointer;
-  box-shadow: 0px 5px 8px 4px #b4b4b476;
   margin-top: 1rem;
   margin-bottom: 0.8rem;
   padding: 0.15rem 5rem;
+  border: solid 1px rgba(128, 128, 128, 0.168);
 }
 </style>
