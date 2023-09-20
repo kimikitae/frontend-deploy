c8f6924c9367334fa40cf604357bf228a54be961
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-09-20 16:26:40 +0900

    ?

--- src/components/chat/ChatModal.vue
+++ src/components/chat/ChatModal.vue
@@ -137,6 +137,9 @@ export default {
       searchtext: "",
       seletedRoom: 0,
       subscription: {},
+      tok:{
+        Authorization: this.tok,
+      }
     };
   },
   computed: {
@@ -183,10 +186,7 @@ export default {
           content: this.message,
         };
         const s = "/send/" + this.seletedRoom;
-        this.stompClient.send(s, JSON.stringify(msg), {});
-        //  {
-        //   Authorization: this.tok,
-        // }
+        this.stompClient.send(s, this.tok, JSON.stringify(msg), {});
       }
     },
     connect() {
@@ -195,7 +195,7 @@ export default {
       this.stompClient = Stomp.over(socket);
       console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`);
       this.stompClient.connect(
-        {},
+        this.tok,
         (frame) => {
           // 소켓 연결 성공
           this.connected = true;
@@ -203,7 +203,7 @@ export default {
           // 서버의 메시지 전송 endpoint를 구독합니다.
           // 이런형태를 pub sub 구조라고 합니다.
           const s = "/chats/" + this.seletedRoom;
-          this.subscription = this.stompClient.subscribe(s, (res) => {
+          this.subscription = this.stompClient.subscribe(s, this.tok, (res) => {
             console.log("구독으로 받은 메시지 입니다.", res.body);
             // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
             let d = JSON.parse(res.body)
