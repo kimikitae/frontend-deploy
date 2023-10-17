f1bacb1347c68df66c6fe76646888ccc5562b2f6
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-10-17 17:45:17 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import PayInfoView from '../views/content/PayInfoView.vue'
 import ShopInfoView from '../views/content/ShopInfoView.vue'
 import RedirectPage from '../components/common/RedirectPage.vue'
 import RedirectPageNaver from '../components/common/RedirectPageNaver.vue'
+import RedirectPageGoogle from '../components/common/RedirectPageGoogle.vue'
 // import store from '../store/index'
 
 const router = createRouter({
@@ -111,6 +112,11 @@ const router = createRouter({
       path: '/redirectn',
       name: 'RedirectPageNaver',
       component: RedirectPageNaver
+    },
+    { // 구글
+      path: '/redirectg',
+      name: 'RedirectPageGoogle',
+      component: RedirectPageGoogle
     },
   ]
 })
