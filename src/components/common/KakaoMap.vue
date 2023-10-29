29078437e65e437d680c102724cd26c474fce297
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2023-10-30 00:43:50 +0900

    Update KakaoMap.vue

--- src/components/common/KakaoMap.vue
+++ src/components/common/KakaoMap.vue
@@ -3,30 +3,17 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
     name: "KakaoMap",
     data() {
         return {
             map: null,
-            list: [{
-                latitude: "37.39843974939604",
-                longitude: "127.10972941510465",
-            },
-            {
-                latitude: "37.32133321",
-                longitude: "127.432432",
-            }
-            ]
         }
     },
-    mounted() {
-        if (window.kakao && window.kakao.maps) {
-            // console.log("if")
-            this.loadMap();
-        } else {
-            // console.log("else")
-            this.loadScript();
-        }
+    computed:{
+        ...mapState('')
     },
     methods: {
         loadScript() {
@@ -48,11 +35,11 @@ export default {
             var positions = [
                 {
                     title: '내 위치',
-                    latlng: new window.kakao.maps.LatLng(35.454078303743316, 128.80681010423675)
+                    latlng: null
                 },
                 {
-                    title: '오칠구 포차',
-                    latlng: new window.kakao.maps.LatLng(35.45403078770194, 128.80561956108846)
+                    title: '공유 장소',
+                    latlng: null
                 },
             ];
 
@@ -61,42 +48,62 @@ export default {
             var imageSize = new window.kakao.maps.Size(30, 40);
             var imageOption = { offset: new window.kakao.maps.Point(0, 0) };
 
-            for (var i = 0; i < positions.length; i++) {
-                // 마커 이미지의 이미지 크기 입니다
+            // 주소-좌표 변환 객체를 생성합니다
+            var geocoder = new window.kakao.maps.services.Geocoder();
 
-                // 마커 이미지를 생성합니다    
-                var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+            // 주소로 좌표를 검색합니다
+            geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function (result, status) {
 
-                // 마커를 생성합니다
-                var marker = new window.kakao.maps.Marker({
-                    map: this.map, // 마커를 표시할 지도
-                    position: positions[i].latlng, // 마커의 위치
-                    title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-                    image: markerImage // 마커 이미지 
-                });
-                // 마커가 지도 위에 표시되도록 설정합니다
-                marker.setMap(this.map);
+                // 정상적으로 검색이 완료됐으면 
+                if (status === window.kakao.maps.services.Status.OK) {
 
-                var content =
-                    '<div class="customoverlay" > ' +
+                    var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+                    // 마커 이미지를 생성합니다    
+                    var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize, imageOption)
+
+                    // 마커를 생성합니다
+                    var marker = new window.kakao.maps.Marker({
+                        map: this.map, // 마커를 표시할 지도
+                        position: positions[i].latlng, // 마커의 위치
+                        title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+                        image: markerImage // 마커 이미지 
+                    });
+                    // 마커가 지도 위에 표시되도록 설정합니다
+                    marker.setMap(this.map);
+
+                    var content =
+                        '<div class="customoverlay" > ' +
                         positions[i].title +
-                    '</div>';
+                        '</div>';
 
-                // 커스텀 오버레이가 표시될 위치입니다 
-                var position = positions[i].latlng;
+                    // 커스텀 오버레이가 표시될 위치입니다 
+                    var position = positions[i].latlng;
 
-                // 커스텀 오버레이를 생성합니다
-                var customOverlay = new window.kakao.maps.CustomOverlay({
-                    map: this.map,
-                    position: position,
-                    content: content,
-                    yAnchor: 1
-                });
+                    // 커스텀 오버레이를 생성합니다
+                    var customOverlay = new window.kakao.maps.CustomOverlay({
+                        map: this.map,
+                        position: position,
+                        content: content,
+                        yAnchor: 1
+                    });
 
-                console.log(customOverlay)
-            }
-        },
-    }
+
+                    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                    this.map.setCenter(coords);
+                }
+            });
+        },      
+    },
+    mounted() {
+        if (window.kakao && window.kakao.maps) {
+            // console.log("if")
+            this.loadMap();
+        } else {
+            // console.log("else")
+            this.loadScript();
+        }
+    },
 };
 </script>
 
